a80da37ad2b60b3c49b657d5384553bf10066e3d
AndroidHQ254 <d.murimi14@gmail.com>
2025-03-29 09:47:50 +0800

    Initial MiuiCamera for toco
    Signed-off-by: AndroidHQ254 <d.murimi14@gmail.com>

---
+++ miuicamera-toco-vendor.mk
@@ -0,0 +1,11 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/miuicamera-toco
+
+PRODUCT_PACKAGES += \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    MiuiCamera
