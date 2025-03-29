85d586f1cf7a92eb0b431a462a3b565276efa68a
AndroidHQ254 <d.murimi14@gmail.com>
2025-03-29 09:56:55 +0800

    Fix Documents mode
    * Include mimoji permissions
    * Kang missing libs
    
    Signed-off-by: AndroidHQ254 <d.murimi14@gmail.com>

--- miuicamera-toco-vendor.mk
+++ miuicamera-toco-vendor.mk
@@ -7,5 +7,28 @@ PRODUCT_SOONG_NAMESPACES += \
 
 PRODUCT_PACKAGES += \
     libcamera_algoup_jni.xiaomi \
+    libcamera_jpegutil_jni.xiaomi \
     libcamera_mianode_jni.xiaomi \
-    MiuiCamera
+    libdoc_photo \
+    libdoc_photo_c++_shared \
+    libgallery_arcsoft_dualcam_refocus \
+    libgallery_arcsoft_portrait_lighting \
+    libgallery_arcsoft_portrait_lighting_c \
+    libgallery_mpbase \
+    libmibokeh_gallery \
+    librefocus \
+    librefocus_mibokeh \
+    libSNPE \
+    MiuiCamera \
+    MiuiExtraPhoto
+
+PRODUCT_PACKAGES += \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_libdoc_photo_so \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_libdoc_photo_c++_shared_so \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_libgallery_arcsoft_dualcam_refocus_so \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_libgallery_arcsoft_portrait_lighting_so \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_libgallery_arcsoft_portrait_lighting_c_so \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_libgallery_mpbase_so \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_libmibokeh_gallery_so \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_librefocus_so \
+    system_priv-app_MiuiExtraPhoto_lib_arm64_librefocus_mibokeh_so
